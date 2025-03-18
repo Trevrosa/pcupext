9a6ba2dd269f26634611a29fe4c8a69908d2f538
trev <trev@trevrosa.dev>
2025-03-18 22:03:41 +0800

    123

--- src/lib/Car.svelte
+++ src/lib/Car.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     let input = $state("");
+    let { name } = $props();
 </script>
 
 <input bind:value={input} />
@@ -9,5 +10,5 @@
         console.error(1);
     }}
 >
-    click me!</button
+    click me, {name}!</button
 >
