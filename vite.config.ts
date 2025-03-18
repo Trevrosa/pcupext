eae79430e018336e497dffaebbdd3e4fbca3a320
trev <trev@trevrosa.dev>
2025-03-18 22:07:12 +0800

    brtli

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-    plugins: [sveltekit(), enhancedImages()],
+    plugins: [sveltekit(), enhancedImages(), viteCompression({ algorithm: "brotliCompress" })],
 });
